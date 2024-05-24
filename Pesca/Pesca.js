ec2fc8f1e080061c4dfe24e01d7a10a129c892d3
SaraMM3 <sara.martinezmolina@estudiante.uam.es>
2024-05-24 18:28:48 +0200

    Intento encontrar fallo pesca

--- Pesca/Pesca.js
+++ Pesca/Pesca.js
@@ -441,7 +441,7 @@ class Example extends Phaser.Scene {
         // Destruimos todos los sprites para volver a iniciar
         let allSprites = this.children.list.filter(x => x instanceof Phaser.GameObjects.Sprite && spritesReset.includes(x.texture.key));
         let debug = []
-        this.children.forEach( x => {
+        this.children.list.forEach( x => {
             debug.push(x.texture.key)
         });
         console.log("debug: " + debug)

